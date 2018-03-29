c7694294a40eeb7e640556e95c2149a7883d00ad
koumoua <37908340+koumoua@users.noreply.github.com>
2018-03-29 11:02:15 -0500

    Testing GitHub
    Lab 11 GitHub

---
+++ EmployeeDictionary/EmployeeDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new Dictionary<int, Employee>();
+
+            employees.Add(101, new Employee { Id = 101, FirstName = "Jesse", LastName = "LIberty" });
+            employees.Add(102, new Employee { Id = 102, FirstName = "George", LastName = "Washington" });
+            employees.Add(103, new Employee { Id = 103, FirstName = "Mark", LastName = "Smith" });
+
+            Console.WriteLine(101);
+            Console.WriteLine(102);
+            Console.WriteLine(103);
+            Console.Read();
+
+        }
+    }
+}
